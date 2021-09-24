08d78bf1e216397359f82cf2011a8af12a236e7e
edunga1 <goonr21@gmail.com>
2021-09-25 02:14:39 +0900

    Clean up code

--- classicassist/feed_pet.py
+++ classicassist/feed_pet.py
@@ -7,41 +7,47 @@ pet_meat = [
     0x317, # beetle
     0xa9, # fire beetle
     0x509, # najasaurus
-    0x994, # pear
 ]
 item_vegetables = [
     0x9d0, # apple
     0x9d2, # peach
+    0x9d1, # grape
+    0x994, # pear
 ]
 item_meats = [
     0x9c9, # ham
 ]
 
-# check pet
-if not FindAlias('pet') or not FindObject('pet'):
-    PromptAlias('pet')
+def adjust_pet():
+    if not FindAlias('pet') or not FindObject('pet'):
+        PromptAlias('pet')
+    return GetAlias('pet')
 
-pet_current = GetAlias('pet')
-pet_graphic = Graphic(pet_current)
-
-def findOneByTypes(types):
+def find_one_by_types(types):
     for type in types:
         if FindType(type, -1, 'backpack'):
             return True
     return False
 
-def main():
+def feed_pet(pet_serial):
     current_foods = []
+    pet_graphic = Graphic(pet_serial)
 
     if pet_graphic in pet_meat:
         current_foods = item_meats
     elif pet_graphic in pet_vegetables:
         current_foods = item_vegetables
 
-    if not findOneByTypes(current_foods):
+    if not find_one_by_types(current_foods):
         HeadMsg("no foods")
-        return
+        return False
 
-    Feed(pet_current, Graphic('found'), 1)
+    Feed(pet_serial, Graphic('found'), 1)
+
+    return True
+
+def main():
+    pet_serial = adjust_pet()
+    feed_pet(pet_serial)
 
 main()
